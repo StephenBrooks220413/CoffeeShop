8fa832626d7b278cea96fd551fecdccc903f7d3d
StephenBrooks220413 <stephen.brooks220413@gmail.com>
2020-10-26 13:23:26 -0500

    first commit

---
+++ js/home.js
@@ -0,0 +1,7 @@
+function info(){
+  document.getElementById("info").innerHTML = "Chicago, New York, Dallas, San Antonio, New Orleans, Atlanta";
+}
+
+function reset(){
+  document.getElementById("info").innerHTML = "Locations Shown Here";
+}
